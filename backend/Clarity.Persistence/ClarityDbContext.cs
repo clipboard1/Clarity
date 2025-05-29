d2f7bc48470025a474d3e2d8adaad1546c859160
Sergey <simon.serezha1@yandex.ru>
2025-05-29 19:17:43 +0500

    feat(persistence): add user model, config. add dbcontext

---
+++ backend/Clarity.Persistence/ClarityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Clarity.Core.Models;
+using Clarity.Persistence.Entitites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clarity.Persistence;
+
+public class ClarityDbContext(DbContextOptions<ClarityDbContext> options) : DbContext(options)
+{
+    public DbSet<UserEntity> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClarityDbContext).Assembly);
+    }
+}
---
+++ backend/Clarity.Persistence/Configurations/UserEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Clarity.Core.Models;
+using Clarity.Persistence.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Clarity.Persistence.Configurations;
+
+public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder
+            .HasKey(u => u.Id);
+        
+        builder
+            .Property(u => u.Username)
+            .HasMaxLength(User.MAX_USERNAME_LENGTH)
+            .IsRequired();
+        
+        builder
+            .Property(u => u.Email)
+            .IsRequired();
+        
+        builder
+            .Property(u => u.PasswordHash)
+            .IsRequired();
+    }
+}
