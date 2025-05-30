8429237a7455f049cdbd87ef34cd18677ffce713
Sergey <simon.serezha1@yandex.ru>
2025-05-30 10:33:56 +0500

    feat: add auth interfaces

---
+++ backend/Clarity.Core/Abstractions/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using Clarity.Core.Models;
+
+namespace Clarity.Core.Abstractions;
+
+public interface IJwtProvider
+{
+    Result<string> GenerateAuthToken(User user);
+    Result<string> GenereateRefreshToken();
+}
---
+++ backend/Clarity.Persistence/Abstractions/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using Clarity.Core.Models;
+using Clarity.Persistence.Entitites;
+
+namespace Clarity.Persistence.Abstractions;
+
+public interface IUsersRepository
+{
+    Task<Result> Add(User user, CancellationToken cancellationToken = default);
+    Task <Result<User>> GetById(Guid id, CancellationToken cancellationToken = default);
+    Task <Result<User>> GetByEmail(string email, CancellationToken cancellationToken = default);
+    
+    Task<Result> SaveRefreshToken(Guid userId, string token, CancellationToken cancellationToken = default);
+    Task<Result<RefreshTokenEntity>> GetRefreshToken(string token, CancellationToken cancellationToken = default);
+    Task <Result> UpdateRefreshToken(RefreshTokenEntity newRefreshToken, CancellationToken cancellationToken = default);
+}
