fb570d7b3fed3a3b1aa4adf9e3ca2eec8eac1a31
Sergey <simon.serezha1@yandex.ru>
2025-06-03 16:07:10 +0500

    feat(application): add command dispatcher

---
+++ backend/Clarity.Application/Abstractions/ICommandDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using Clarity.Core.Models;
+
+namespace Clarity.Application.Abstractions;
+
+public interface ICommandDispatcher
+{
+    Task<Result> DispatchAsync<TCommand>(TCommand command) where TCommand : ICommand;
+}
+
+public interface ICommandDispatcher<TResponse>
+{
+    Task<Result<TResponse>> DispatchAsync<TCommand>(TCommand command) where TCommand 
+        : ICommand<TResponse>;
+}
---
+++ backend/Clarity.Application/CommandDispatcher.cs
@@ -0,0 +1,44 @@
+﻿using Clarity.Application.Abstractions;
+using Clarity.Core.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clarity.Application;
+
+public class CommandDispatcher : ICommandDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public CommandDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    
+    public async Task<Result> DispatchAsync<TCommand>(TCommand command) where TCommand : ICommand
+    {
+        var handler = _serviceProvider.GetService<ICommandHandler<TCommand>>();
+        if (handler == null)
+            throw new InvalidOperationException($"Handler for command {typeof(TCommand).Name} not found.");
+
+        return await handler.Handle(command);
+    }
+}
+
+public class CommandDispatcher<TResponse> : ICommandDispatcher<TResponse>
+{
+    private readonly IServiceProvider _serviceProvider;
+    
+    public CommandDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    
+    public async Task<Result<TResponse>> DispatchAsync<TCommand>(TCommand command) where TCommand : ICommand<TResponse>
+    {
+        var handlerType = typeof(ICommandHandler<,>).MakeGenericType(command.GetType(), typeof(TResponse));
+        dynamic handler = _serviceProvider.GetService(handlerType);
+        if (handler == null)
+            throw new InvalidOperationException($"Handler for command {typeof(TCommand).Name} not found.");
+
+        return await handler.Handle(command);
+    }
+}
