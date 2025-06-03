4a63c38748d455293b659466c6feb14c0004fee8
Sergey <simon.serezha1@yandex.ru>
2025-06-03 16:06:38 +0500

    feat(api): add contracts

---
+++ backend/Clarity.Api/Contracts/Users/LoginUserRequest.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clarity.Api.Contracts.Users;
+
+public record LoginUserRequest(
+    [Required] string Email,
+    [Required] string Password);
---
+++ backend/Clarity.Api/Contracts/Users/RegisterUserRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clarity.Api.Contracts.Users;
+
+public record RegisterUserRequest(
+    [Required] string Username,
+    [Required] string Email,
+    [Required] string Password);
