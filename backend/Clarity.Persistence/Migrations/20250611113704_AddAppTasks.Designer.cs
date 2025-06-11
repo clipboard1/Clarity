e12e38b0df808ecfa9d51763445c0a3124f5c743
Sergey <simon.serezha1@yandex.ru>
2025-06-11 17:09:42 +0500

    fix(persistence): add forgotten migrations

--- backend/Clarity.Persistence/Migrations/20250611113704_AddAppTasks.Designer.cs
+++ backend/Clarity.Persistence/Migrations/20250611113704_AddAppTasks.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Clarity.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Clarity.Persistence.Migrations
 {
     [DbContext(typeof(ClarityDbContext))]
-    partial class ClarityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250611113704_AddAppTasks")]
+    partial class AddAppTasks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Clarity.Persistence/Migrations/20250611113704_AddAppTasks.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Clarity.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppTasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Deadline = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppTasks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    AppTaskId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskTagEntity",
+                columns: table => new
+                {
+                    AppTaskId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskTagEntity", x => new { x.AppTaskId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_TaskTagEntity_AppTasks_AppTaskId",
+                        column: x => x.AppTaskId,
+                        principalTable: "AppTasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskTagEntity_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppTasks_UserId",
+                table: "AppTasks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskTagEntity_TagId",
+                table: "TaskTagEntity",
+                column: "TagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskTagEntity");
+
+            migrationBuilder.DropTable(
+                name: "AppTasks");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
