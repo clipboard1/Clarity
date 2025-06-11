4b54af9f58c08ff32a595261d71b187ed2c9cb34
Sergey <simon.serezha1@yandex.ru>
2025-06-11 17:21:09 +0500

    feat(persistence): add get tasks queries

---
+++ backend/Clarity.Application/AppTasks/GetAll/GetAllTasksQuery.cs
@@ -0,0 +1,27 @@
+﻿using Clarity.Application.Abstractions;
+using Clarity.Core.Models;
+using Clarity.Persistence.Abstractions;
+
+namespace Clarity.Application.AppTasks.GetAll;
+
+public record GetAllTasksQuery(Guid UserId)
+    : IQuery<List<AppTask>>;
+
+public class GetAllTasksQueryHandler : IQueryHandler<GetAllTasksQuery, List<AppTask>>
+{
+    private readonly IAppTasksRepository _repository;
+
+    public GetAllTasksQueryHandler(IAppTasksRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Result<List<AppTask>>> Handle(GetAllTasksQuery tasksQuery)
+    {
+        var getResult = await _repository.GetAll(tasksQuery.UserId);
+        if (!getResult.IsSuccess)
+            return Result<List<AppTask>>.Failure(getResult.Errors);
+        
+        return Result<List<AppTask>>.Success(getResult.Value);
+    }
+}
---
+++ backend/Clarity.Application/AppTasks/GetById/GetTaskByIdQuery.cs
@@ -0,0 +1,27 @@
+﻿using Clarity.Application.Abstractions;
+using Clarity.Core.Models;
+using Clarity.Persistence.Abstractions;
+
+namespace Clarity.Application.AppTasks.GetById;
+
+public record GetTaskByIdQuery(Guid Id)
+    : IQuery<AppTask>;
+    
+public class GetTaskByIdQueryHandler : IQueryHandler<GetTaskByIdQuery, AppTask>
+{
+    private readonly IAppTasksRepository _repository;
+
+    public GetTaskByIdQueryHandler(IAppTasksRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Result<AppTask>> Handle(GetTaskByIdQuery query)
+    {
+        var getResult = await _repository.GetById(query.Id);
+        if (!getResult.IsSuccess)
+            return Result<AppTask>.Failure(getResult.Errors);
+        
+        return Result<AppTask>.Success(getResult.Value);
+    }
+}
