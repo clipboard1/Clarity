3c2106413d9ebc17b4665ae489c34a89df0518b8
Sergey <simon.serezha1@yandex.ru>
2025-06-11 17:15:08 +0500

    feat(application): add query dispatcher

---
+++ backend/Clarity.Application/Abstractions/IQueryDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using Clarity.Core.Models;
+
+namespace Clarity.Application.Abstractions;
+
+public interface IQueryDispatcher
+{
+    Task<Result<TResponse>> DispatchAsync<TQuery, TResponse>(TQuery query)
+        where TQuery : IQuery<TResponse>;
+}
---
+++ backend/Clarity.Application/QueryDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using Clarity.Application.Abstractions;
+using Clarity.Core.Models;
+
+namespace Clarity.Application;
+
+public class QueryDispatcher : IQueryDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public QueryDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task<Result<TResponse>> DispatchAsync<TQuery, TResponse>(TQuery query) 
+        where TQuery : IQuery<TResponse>
+    {
+        var handlerType = typeof(IQueryHandler<,>).MakeGenericType(query.GetType(), typeof(TResponse));
+        dynamic handler = _serviceProvider.GetService(handlerType);
+        if (handler == null)
+            throw new InvalidOperationException($"Handler for command {typeof(TQuery).Name} not found.");
+
+        return await handler.Handle(query);
+    }
+}
