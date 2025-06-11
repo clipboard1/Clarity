1cfc7669fa383c2140c3ae49176928f62cfc6da5
Sergey <simon.serezha1@yandex.ru>
2025-06-11 17:16:31 +0500

    feat(persistence): add extension for mapping from entity to domain model(temp)

---
+++ backend/Clarity.Persistence/Extensions/AppTaskEntityExtension.cs
@@ -0,0 +1,17 @@
+﻿using Clarity.Core.Models;
+using Clarity.Persistence.Entitites;
+
+namespace Clarity.Persistence.Extensions;
+
+public static class AppTaskEntityExtension
+{
+    public static AppTask ToDomainModel(this AppTaskEntity entity)
+        => AppTask.Create(entity.Id, entity.UserId, entity.Title, 
+            entity.Description,entity.CreationDate, 
+            entity.Deadline, entity.Tags.Select(tt => tt.ToDomainModel()).ToList(), 
+            entity.Status).Value;
+    
+    public static Tag ToDomainModel(this TagEntity entity)
+        => new Tag { Id = entity.Id, Name = entity.Name, AppTaskId = entity.AppTaskId };
+
+}
