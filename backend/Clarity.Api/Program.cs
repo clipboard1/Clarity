35c72470e1d7c79b9ab58e36796da217bc6d222f
Sergey <simon.serezha1@yandex.ru>
2025-06-11 17:21:46 +0500

    feat(api): register repo, commands and queries

--- backend/Clarity.Api/Program.cs
+++ backend/Clarity.Api/Program.cs
@@ -1,6 +1,11 @@
 using Clarity.Api.Extensions;
 using Clarity.Application;
 using Clarity.Application.Abstractions;
+using Clarity.Application.AppTasks.Create;
+using Clarity.Application.AppTasks.Delete;
+using Clarity.Application.AppTasks.GetAll;
+using Clarity.Application.AppTasks.GetById;
+using Clarity.Application.AppTasks.Update;
 using Clarity.Application.LogoutAllDevices;
 using Clarity.Application.Users.Login;
 using Clarity.Application.Users.LoginByRefreshToken;
@@ -10,6 +15,7 @@ using Clarity.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Clarity.Core.Abstractions;
 using Clarity.Core.Dto;
+using Clarity.Core.Models;
 using Clarity.Infrastructure.Authentication;
 using Clarity.Persistence.Abstractions;
 using Clarity.Persistence.Repositories;
@@ -34,13 +40,22 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
 builder.Services.AddSingleton<IJwtProvider, JwtProvider>();
 builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<IAppTasksRepository, AppTasksRepository>();
 
 builder.Services.AddScoped<ICommandHandler<RegisterCommand>, RegisterCommandHandler>();
 builder.Services.AddScoped<ICommandHandler<LoginCommand, AuthTokens>, LoginCommandHandler>();
 builder.Services.AddScoped<ICommandHandler<LogoutCommand>, LogoutCommandHandler>();
 builder.Services.AddScoped<ICommandHandler<LogoutAllDevicesCommand>, LogoutAllDevicesCommandHandler>();
 builder.Services.AddScoped<ICommandHandler<LoginByRefreshTokenCommand, AuthTokens>,LoginByRefreshTokenCommandHandler>();
+
+builder.Services.AddScoped<ICommandHandler<CreateTaskCommand, Guid>, CreateTaskCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<UpdateTaskCommand>, UpdateTaskCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<DeleteTaskCommand>, DeleteTaskCommandHandler>();
+builder.Services.AddScoped<IQueryHandler<GetAllTasksQuery, List<AppTask>>, GetAllTasksQueryHandler>();
+builder.Services.AddScoped<IQueryHandler<GetTaskByIdQuery, AppTask>, GetTaskByIdQueryHandler>();
+
 builder.Services.AddScoped<ICommandDispatcher, CommandDispatcher>();
+builder.Services.AddScoped<IQueryDispatcher, QueryDispatcher>();
 
 builder.Services.AddApiAuthentication(
     builder.Services
