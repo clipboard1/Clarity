998a34a2c329df74585249e0fbb42f7ea0d555fa
Sergey <simon.serezha1@yandex.ru>
2025-06-11 17:20:00 +0500

    feat(persistence): add delete task command

---
+++ backend/Clarity.Application/AppTasks/Delete/DeleteTaskCommand.cs
@@ -0,0 +1,31 @@
+﻿using Clarity.Application.Abstractions;
+using Clarity.Core.Models;
+using Clarity.Persistence.Abstractions;
+
+namespace Clarity.Application.AppTasks.Delete;
+
+public record DeleteTaskCommand(Guid Id)
+    : ICommand;
+    
+public class DeleteTaskCommandHandler : ICommandHandler<DeleteTaskCommand>
+{
+    private readonly IAppTasksRepository _repository;
+
+    public DeleteTaskCommandHandler(IAppTasksRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task<Result> Handle(DeleteTaskCommand taskCommand)
+    {
+        var getResult = await _repository.GetById(taskCommand.Id);
+        if (!getResult.IsSuccess)
+            return Result.Failure(getResult.Errors);
+
+        var deleteResult = await _repository.Delete(taskCommand.Id);
+        if (!deleteResult.IsSuccess)
+            return Result.Failure(deleteResult.Errors);
+        
+        return Result.Success();
+    }
+}
