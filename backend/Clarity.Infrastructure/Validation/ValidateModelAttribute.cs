5ef1508089abbafd6be8248c11854b1a7ba88136
Sergey <simon.serezha1@yandex.ru>
2025-06-17 09:51:53 +0500

    feat(infrastructure): add fluent validation attribute

---
+++ backend/Clarity.Infrastructure/Validation/ValidateModelAttribute.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clarity.Infrastructure.Validation;
+
+public class ValidateModelAttribute<T> : ActionFilterAttribute
+{
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        foreach (var argument in context.ActionArguments)
+        {
+            if (argument.Value is T model)
+            {
+                var validator = context.HttpContext.RequestServices.GetService<IValidator<T>>();
+                if (validator != null)
+                {
+                    var validationResult = await validator.ValidateAsync(model);
+                    if (!validationResult.IsValid)
+                    {
+                        context.Result = new BadRequestObjectResult(new ValidationProblemDetails
+                        {
+                            Errors = validationResult.Errors
+                                .GroupBy(e => e.PropertyName)
+                                .ToDictionary(
+                                    el => el.Key,
+                                    el => 
+                                        el.Select(p => p.ErrorMessage)
+                                            .ToArray())
+                        });
+                        return;
+                    }
+                }
+                break;
+            } 
+        }
+        await next();
+    }
+}
